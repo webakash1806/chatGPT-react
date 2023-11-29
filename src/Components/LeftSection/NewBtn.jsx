e61a786aa073e7562910eb520b711ebc5ce889b3
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-11-29 21:03:06 +0530

    style updated

--- src/Components/LeftSection/NewBtn.jsx
+++ src/Components/LeftSection/NewBtn.jsx
@@ -4,9 +4,10 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 const NewBtn = () => {
     return (
-        <div className='flex items-center justify-center w-full p-2 gap-3 rounded-[5px] bg-[#6366F1] text-white font-medium tracking-wide'>
+        <div className='flex items-center justify-center w-full  cursor-pointer p-2 gap-3 rounded-[5px] bg-[#6366F1] text-white font-medium tracking-wide'
+            onClick={() => window.location.reload()}>
             <FontAwesomeIcon icon={faPlus} />
-            <p>New Chat</p>
+            <p className='pointer-events-none'>New Chat</p>
         </div>
     )
 }
--- src/Components/RightSection/SearchBox.jsx
+++ src/Components/RightSection/SearchBox.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 
 const SearchBox = () => {
     return (
         <div>
-            <textarea name="" id="" cols="50" rows="5"></textarea>
+            <textarea
+                className='text-black'
+                name="" id="" cols="50" rows="5"></textarea>
+            <FontAwesomeIcon icon={faPaperPlane} />
         </div>
     )
 }
