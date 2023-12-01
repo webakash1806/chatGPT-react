def597ff1934a7b159ceba58ec0b56d70181efb0
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-12-01 20:04:27 +0530

    styles updated

--- src/Components/Response/Response.jsx
+++ src/Components/Response/Response.jsx
@@ -4,7 +4,7 @@ import { faUser, faCommentDots } from '@fortawesome/free-solid-svg-icons'
 
 const Response = ({ text, isAI }) => {
     return (
-        <div className={`${isAI ? 'bg-transparent' : 'bg-[#4a4a4a63] mb-5 border border-gray-700 border-b-[3px] pt-3 pb-5 rounded-md'} max-w-[50rem] p-[8px] mr-2`}>
+        <div className={`${isAI ? 'bg-transparent' : 'bg-[#4a4a4a63] mb-6 border border-gray-700 border-b-[3px] pt-3 pb-4 rounded-md'} w-[92vw] sm:w-[88.5vw] md:min-w-[47rem] md:max-w-[47rem] lg:min-w-[45rem] lg:max-w-[45rem] p-[7px] mr-2`}>
             <p className='pb-1 flex items-center gap-[6px] text-[17px] font-bold tracking-wide '><FontAwesomeIcon className={`${isAI ? 'bg-[#6366f9]' : 'bg-[#00b66a]'} w-[13.5px] h-[13.5px] p-[6.5px] rounded-[30%] `}
                 icon={isAI ? faUser : faCommentDots} />{isAI ? 'You' : 'spaceAI'}</p>
             <p className={`${isAI ? 'font-semibold' : 'tracking-[0.2px] text-[16px]'} pl-[2.1rem]   text-[#e5e5ed] text-[15px]`}>{text}</p>
--- src/Components/RightSection/SearchBox.jsx
+++ src/Components/RightSection/SearchBox.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 import { sendMsg } from '../../Hooks/response'
@@ -8,14 +8,24 @@ import Response from '../Response/Response'
 const SearchBox = () => {
     const [que, setQue] = useState()
     const [answer, setAnswer] = useState([])
+    const msgEnd = useRef(null)
 
-    console.log(answer)
+    // console.log(answer)
 
     const handleSend = async () => {
+        setAnswer([...answer, { text: que, isAI: true }, { text: '...', isAI: false }])
         const res = await sendMsg(que)
-        setAnswer([...answer, { text: que, isAI: true, fill: true }, { text: res, isAI: false, fill: true }])
+        setAnswer([...answer, { text: que, isAI: true }, { text: res, isAI: false }])
         console.log(res)
     }
+
+    const handleEnter = async (e) => {
+        if (e.key == 'Enter') await handleSend()
+    }
+
+    useEffect(() => {
+        msgEnd.current.scrollIntoView()
+    }, [answer])
 
     return (
         <div className='relative'>
@@ -25,8 +35,9 @@ const SearchBox = () => {
                         return <Response key={i + 1} text={response.text} isAI={response.isAI} />
                     })
                 }</>}
+                <div ref={msgEnd} />
             </div>
-            <textarea onChange={(e) => setQue(e.target.value)}
+            <textarea onKeyDown={handleEnter} onChange={(e) => setQue(e.target.value)}
                 className='text-white bg-[#222229] mt-6 w-[95vw] sm:w-[90vw] md:min-w-[48rem] md:max-w-[48rem] lg:min-w-[46rem] lg:max-w-[46rem] rounded-lg resize-none border border-slate-600  shadow-sm shadow-[#6365f1e4] outline-none p-[5px_10px]'
                 name="" id="" rows="2"
                 placeholder='Search Something...'></textarea>
